a6a1791a280279b2e887a26d02e441f4a7f6d19c
Ishita Kabra <ishitakabra1805@gmail.com>
2021-05-12 19:45:52 +0530

    config for online db

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_OWNER_ID, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
+const db = new Sequelize(process.env.DB_NAME_PROD, process.env.DB_OWNER_ID_PROD, process.env.DB_PASS_PROD, {
+  host: process.env.DB_HOST_PROD,
   dialect: process.env.DB_DIALECT,
   operatorAliases: false,
   logging: false,
